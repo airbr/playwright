10c1e2a1ca6c4a9045dc80b97f9050c672165e06
Morgan Murrah <morgan@morganwebdev.com>
2023-09-27 21:43:14 -0700

    Update; Test CSS Locator

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,8 +15,7 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -25,7 +24,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:1313/',
-    trace: 'on',
+    trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
@@ -54,16 +53,20 @@ export default defineConfig({
       name: 'Mobile Safari',
       use: { ...devices['iPhone 12'] },
     },
+    {
+      name: 'Moto G4',
+      use: {...devices['Moto G4'] }
+    },
 
     /* Test against branded browsers. */
-    {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    },
-    {
-      name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    },
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
   ],
 
   /* Run your local dev server before starting the tests */
--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -2,24 +2,20 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://www.morganwebdev.org/');
-
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Morgan Murrah/);
 });
 
-test('3 images on main page', async ({ page }) => {
+test('Layout and images of main page: 6 headlines, 3 images', async ({ page }) => {
   await page.goto('https://www.morganwebdev.org/');
-
-  await expect(page.locator('css=img')).toHaveCount(3);
+  await expect(page.locator('css=.img')).toHaveCount(3);
+  await expect(page.locator('css=h1')).toHaveCount(6);
 });
-
 
 test('About me page loads and title', async ({ page }) => {
   await page.goto('https://www.morganwebdev.org/');
-
   // Click the get started link.
   await page.getByRole('link', { name: 'about me' }).click();
-
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'About me' })).toBeVisible();
 });
