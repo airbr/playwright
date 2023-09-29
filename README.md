34ae0b31b1c8d90cddd0ee3f513805cac0e58faf
Morgan Murrah <morgan@morganwebdev.com>
2023-09-28 20:24:54 -0700

    Update with 123 Devices from Playwright

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # The Playwright Repository for https://morganwebdev.org
 
+This project uses all 123 Devices in the Projects config.
+
+Run an individual project by `npx playwright test --project="Desktop Firefox"` for example.
--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,21 +1,21 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('Homepage quality checks', async ({ page }) => {
   await page.goto('https://www.morganwebdev.org/');
-  // Expect a title "to contain" a substring.
+  await expect(page).toBeDefined();
+  await expect(page).toHaveURL(/.morganwebdev.org/);
   await expect(page).toHaveTitle(/Morgan Murrah/);
-});
-
-test('Layout and images of main page: 6 headlines, 3 images', async ({ page }) => {
-  await page.goto('https://www.morganwebdev.org/');
   await expect(page.locator('css=.img')).toHaveCount(3);
   await expect(page.locator('css=h1')).toHaveCount(6);
+  await expect(page.getByRole('heading', { name: 'Morgan Murrah' })).toBeVisible();
+  await expect(page.getByTitle('Github link')).toHaveCount(2);
+  await expect(page.getByTitle('RSS link')).toHaveCount(2);
 });
 
-test('About me page loads and title', async ({ page }) => {
-  await page.goto('https://www.morganwebdev.org/');
-  // Click the get started link.
-  await page.getByRole('link', { name: 'about me' }).click();
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'About me' })).toBeVisible();
-});
+// test('About me page loads and title', async ({ page }) => {
+//   await page.goto('https://www.morganwebdev.org/');
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'about me' }).click();
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'About me' })).toBeVisible();
+// });
